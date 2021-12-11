d317c62ab053ab2d54ea5ba7b26a7b5774e4836e
Lidija Levchenko <levchenkola@ukr.net>
2021-12-11 22:23:51 +0200

    Update 01-gallery.js

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,3 +2,12 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+
+const  gallery = document.querySelector('gallery');
+const listNew = galleryItems
+ .map(galleryItems => `<li class="gallery__item">
+                     <img class="gallery__img" src="${galleryItems.preview}" alt="${galleryItems.description}" width="630" height="375">
+                 </li>`);
+//gallery.addEventListener('click' );
+//gallery.insertAdjacentHTML("afterbegin", listNew)
