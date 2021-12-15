c6c48a02a917738b8e2a599ff14c92a20371dbc2
Lidija Levchenko <levchenkola@ukr.net>
2021-12-15 22:57:08 +0200

    +

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -5,7 +5,7 @@ console.log(galleryItems);
 
 
 const  gallery = document.querySelector('.gallery');
-const bodyEl = document.querySelector('body');
+
 let instance = {};
 
 
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,60 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const  gallery = document.querySelector('.gallery');
+
+
+
+
+// 1.Создание и рендер разметки по массиву данных galleryItems и предоставленному шаблону элемента галереи./
+const listNew = galleryItems.map(({ original, preview, description }) => 
+     `<li class="gallery__item">
+     <a class="gallery__link" href="${original}">
+         <img
+             class="gallery__image"
+             src="${preview}"
+             alt="${description}"
+         />
+     </a>
+ </li>`).join("");;
+
+gallery.insertAdjacentHTML("beforeend", listNew);
+console.log(gallery);
+// 2.Реализация делегирования на div.gallery и получение url большого изображения.
+
+
+// function onClick(evt) {
+//     evt.preventDefault();
+    
+//     if(evt.target.nodeName !== 'IMG'){
+//         return;
+//     }
+//     openModalWindow(evt.target.dataset.source);
+// }
+
+// gallery.addEventListener('click', onClick);
+
+// 3.
+const lightboxOptions = {
+    captions: true,
+    captionsData: "alt",
+    captionDelay: 250,
+  };
+  
+  const lightboxGallery = new SimpleLightbox(".gallery a", lightboxOptions);
+// 4
+// function openModalWindow(pic) {
+//   instance = onCreateModal(pic);
+//   instance.show();
+//   document.addEventListener("keyup", pressEscapeInModal);
+// }
+
+
+//   function pressEscapeInModal(evt) {
+//     if (evt.code !== "Escape") {
+//       return;
+//     }
+//     instance.close();
+//     document.removeEventListener("keyup", pressEscapeInModal);
+//   }
